380a3ee84f27a01b3ad861262a41f6d5088df1a1
Siddharth03072004 <22211a05g5@bvrit.ac.in>
2025-03-01 17:26:25 +0530

    fix: links -2

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -3,12 +3,18 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./Components/Home";
+import Orders from "./Components/Orders";
+import Positions from "./Components/Positions";
+import Holdings from "./Components/Holdings";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/positions" element={<Positions />} />
+        <Route path="/holdings" element={<Holdings />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
