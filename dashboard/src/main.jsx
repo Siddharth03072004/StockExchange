54ac9dfbed317f87277cbe4049dc595a9ccda617
Siddharth03072004 <22211a05g5@bvrit.ac.in>
2025-03-01 17:29:44 +0530

    fix: links -4

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./Components/Home";
-import Orders from "./Components/Orders";
-import Positions from "./Components/Positions";
-import Holdings from "./Components/Holdings";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
