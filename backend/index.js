1a4aee0733a7fad66c463797e439a3da926d55a6
Siddharth03072004 <22211a05g5@bvrit.ac.in>
2025-03-14 13:06:40 +0530

    update cors5

--- backend/index.js
+++ backend/index.js
@@ -21,15 +21,15 @@ const corsOptions = {
     optionsSuccessStatus: 200,
 };
 app.use(bodyParser.json());
-//app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(cors(corsOptions));
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "https://stock-exchange-ds.vercel.app");
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://stock-exchange-ds.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 
 
 // app.get('/addHoldings', async(req, res) => {
